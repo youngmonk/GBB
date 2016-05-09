5f71c17693b808d8aba37a28a169d893b364ff90
SaiNageswarS <sainageswar@gmail.com>
2016-05-09 20:45:52 +0530

    prod setup for db

--- app.py
+++ app.py
@@ -2,13 +2,19 @@ from flask import Flask
 from routes import predictor, root_routes, file_manager
 from gevent.wsgi import WSGIServer
 from models.predicted_prices import db
+import os
 import logging
 
 app = Flask(__name__, static_url_path='/public')
-
+env = os.environ.get('env', 'DEV') # default env is DEV. For prod set an environment variable env as PROD
 
 def init_app():
-    app.config.from_object('config.Config')
+    if env == 'PROD':
+        print('Configuring for prod')
+        app.config.from_object('config.ProductionConfig')
+    else:
+        print('Configuring for develop')
+        app.config.from_object('config.Config')
     db.init_app(app=app)
 
 
@@ -22,7 +28,10 @@ def init_routes():
 if __name__ == "__main__":
     init_app()
     init_routes()
-    # app.run(host='0.0.0.0', port=7000) # debug server
     print('Starting server')
-    http_server = WSGIServer(('', 7000), app)
-    http_server.serve_forever()
+    if env == 'PROD':
+        http_server = WSGIServer(('', 7000), app)
+        http_server.serve_forever()
+    else:
+        app.run(host='0.0.0.0', port=7000) # debug server
+
--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config(object):
-    DEBUG = False
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:zoomo123@localhost:5432/gozoomo'
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/gozoomo?application_name=GBB'
     SQLALCHEMY_POOL_SIZE = 10
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     POOL_SIZE = 10
@@ -9,3 +9,4 @@ class Config(object):
 
 class ProductionConfig(Config):
     DEBUG = False
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:gaeK1oo6ai@pg.gozoomo.com:5432/gozoomo?application_name=GBB'
